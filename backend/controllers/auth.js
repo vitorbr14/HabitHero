a28eba2fef611471c6cdb89080759f336f26b55f
Vitor Gomes <vitorgomes605@gmail.com>
2023-10-05 21:15:14 -0300

    Login Feature

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,37 +1,41 @@
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
-
-
-
-
+const {StatusCodes} = require('http-status-codes')
+const {BadRequestError, UnauthenticatedError } = require('../errors')
 
 
 
   const register = async (req, res) => {
-    const {name,password,email} = req.body
-  
-    const salt = await bcrypt.genSalt(10)
-    const hashedPassword = await bcrypt.hash(password,salt)
-    //mesmo se o password tiver vazio, ele vai enviar pq está enviado o password hasheado, é melhor deixar essa parte no model
-    const newUser = {
-      name,
-      password:hashedPassword,
-      email
-    }
-    
-    const user = await User.create({newUser})
-   res.send(newUser)
+    const user = await User.create({...req.body})
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({name:{name:user.name}, token})
+
   }
   
 
-
-
-
-
-
-
   const login = async (req, res) => {
-   res.send('login')
+    const {email,password} = req.body
+    if(!email || !password) {
+        throw new BadRequestError('Please provide email and password')
+    }
+
+    const user = await User.findOne({email})
+
+    if(!user) {
+      throw new UnauthenticatedError('Invalid credentials')
+    }
+    
+    const isPasswordCorrect = await user.comparePassword(password)
+
+    if(!isPasswordCorrect) {
+      
+      throw new UnauthenticatedError('Invalid credentials')
+      
+    }
+
+    const token = user.createJWT()
+    res.status(StatusCodes.CREATED).json({name:{name:user.name}, token})
+
   }
   
   module.exports = {
--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 
 const UserSchema = new mongoose.Schema({
@@ -25,6 +27,22 @@ const UserSchema = new mongoose.Schema({
        
 
     },
+    
 });
+UserSchema.pre('save', async function(next) {
+  const salt = await bcrypt.genSalt(10)
+  this.password =  await bcrypt.hash(this.password,salt)
+   next();
+ });
+
+
+ UserSchema.methods.createJWT = function () {
+  return jwt.sign({userId: this._id, name:this.name}, process.env.JWT_SECRET,{expiresIn:process.env.JWT_LIFETIME})
+ }
+ 
+ UserSchema.methods.comparePassword = async function(candidatedPassword) {
+  const isMatch = await bcrypt.compare(candidatedPassword,this.password)
+  return isMatch
+ }
 
 module.exports = mongoose.model('User',UserSchema)
