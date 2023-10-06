1ce79dff0d62b74b417d8235cf6dc384f4ba0ea7
Vitor Gomes <vitorgomes605@gmail.com>
2023-10-06 20:26:42 -0300

    Vite

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,44 +1,66 @@
 const User = require('../models/User')
 const bcrypt = require('bcryptjs')
-const {StatusCodes} = require('http-status-codes')
-const {BadRequestError, UnauthenticatedError } = require('../errors')
+const {
+    StatusCodes
+} = require('http-status-codes')
+const {
+    BadRequestError,
+    UnauthenticatedError
+} = require('../errors')
 
 
 
-  const register = async (req, res) => {
-    const user = await User.create({...req.body})
+const register = async (req, res) => {
+    const user = await User.create({
+        ...req.body
+    })
     const token = user.createJWT()
-    res.status(StatusCodes.CREATED).json({name:{name:user.name}, token})
+    res.status(StatusCodes.CREATED).json({
+        name: {
+            name: user.name
+        },
+        token
+    })
 
-  }
-  
+}
 
-  const login = async (req, res) => {
-    const {email,password} = req.body
-    if(!email || !password) {
+
+const login = async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body
+    if (!email || !password) {
         throw new BadRequestError('Please provide email and password')
     }
 
-    const user = await User.findOne({email})
+    const user = await User.findOne({
+        email
+    })
 
-    if(!user) {
-      throw new UnauthenticatedError('Invalid credentials')
+    if (!user) {
+        throw new UnauthenticatedError('Invalid credentials')
     }
-    
+
     const isPasswordCorrect = await user.comparePassword(password)
 
-    if(!isPasswordCorrect) {
-      
-      throw new UnauthenticatedError('Invalid credentials')
-      
+    if (!isPasswordCorrect) {
+
+        throw new UnauthenticatedError('Invalid credentials')
+
     }
 
     const token = user.createJWT()
-    res.status(StatusCodes.CREATED).json({name:{name:user.name}, token})
+    res.status(StatusCodes.CREATED).json({
+        name: {
+            name: user.name
+        },
+        token
+    })
 
-  }
-  
-  module.exports = {
+}
+
+module.exports = {
     register,
     login,
-  }
+}
