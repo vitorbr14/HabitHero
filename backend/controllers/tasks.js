0db6e5ae625a7879103f4bc8d338a1164707b118
Vitor Gomes <vitorgomes605@gmail.com>
2023-10-07 21:38:55 -0300

    All tasks and single task.

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,34 +1,66 @@
-
+const Task = require('../models/Task')
+const {
+    StatusCodes
+} = require('http-status-codes')
+const {
+    BadRequestError,
+    NotFoundError
+} = require('../errors')
 
 const createTask = async (req, res) => {
-    res.send(req.userTokenData)
+    req.body.createdBy = req.userTokenData.userId
+    const task = await Task.create(req.body)
+    res.status(StatusCodes.CREATED).json({
+        task
+    })
 
-  }
-  
-  const getAllTasks = async (req, res) => {
-    res.send('All tasks')
+}
 
-  }
+const getAllTasks = async (req, res) => {
+    const allTasks = await Task.find({
+        createdBy: req.userTokenData.userId
+    })
+    res.json(allTasks)
+}
 
-  const getSingleTask = async (req, res) => {
-    res.send('single Task')
+const getSingleTask = async (req, res) => {
 
-  }
 
-  const updateTask = async (req, res) => {
+    const {
+        id
+    } = req.params
+    const {
+        userId
+    } = req.userTokenData
+    console.log(id)
+    const newTask = await Task.findOne({
+      
+        _id: id,
+        createdBy: userId,
+    });
+
+    if (!newTask) {
+        throw new NotFoundError(`No task found with this ID: ${id}`)
+    }
+    res.send(newTask)
+
+
+}
+
+const updateTask = async (req, res) => {
     res.send('update task')
 
-  }
+}
 
-  const deleteTask = async (req, res) => {
+const deleteTask = async (req, res) => {
     res.send('delete task')
 
-  }
-  
-  module.exports = {
+}
+
+module.exports = {
     createTask,
     getAllTasks,
     getSingleTask,
     updateTask,
     deleteTask
-  }
+}
--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,1 +1,26 @@
 const mongoose = require('mongoose')
+const TaskSchema = new mongoose.Schema({
+    taskTitle: {
+        type: String,
+        required:[true, 'Please provida the task name'],
+        maxlength:50
+    },
+    taskDesc: {
+        type: String,
+        maxlength:150
+    },
+    taskStatus: {
+        type: String,
+        enum:['completed', 'pending'],
+        default: 'pending'
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref:'User',
+        required:[true,'Please provide an User']
+        
+    },
+},{timestamps:true})
+
+
+module.exports = mongoose.model('Task',TaskSchema)
